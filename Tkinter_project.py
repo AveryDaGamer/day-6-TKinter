647c247f8f66642b785666f5ec847d7f30030c2f
ConstantineIIII <kalel.rosenthal@gmail.com>
2026-07-07 16:12:21 -0400

    Update Tkinter_project.py

--- Tkinter_project.py
+++ Tkinter_project.py
@@ -1,4 +1,6 @@
-
-
-
+#1 Clicking a Button -- Handled with "Command=" 
+#2 Typing a key -- Handled with ".bind()"
+#3 Moving the mouse -- Also Handled with ".bind()"
+#4 Selecting a Radiobutton -- Also with "Command=". It checks the shared variable 
+#5 Closing the Window -- Handled with the "window.protocol()"
 
